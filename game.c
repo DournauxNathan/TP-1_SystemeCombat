3081874b55412c0ce23bcba41a897787380d48ae
DOURNAUX Nathan <dournauxnathan@gmail.com>
2020-01-21 13:23:56 +0100

    Vie, attaque, dégâts et actualisation
    Programme affichant un nombre de points de vie à l’écran, puis une phrase
    décrivant une attaque et une perte de points de vie, et enfin le nombre de points de vie actualisé après la blessure.

--- game.c
+++ game.c
@@ -2,6 +2,23 @@
 
 int main()
 {
+	int vie;
+	vie = 30;
+
+	int vieMonstre;
+	vieMonstre = 50;
+	float degat;
+	degat = 5;
+
+	printf("Vous: %d \n", vie);
+
+	printf("Monstre: %d \n \n", vieMonstre);
+
+	printf("Vous attaquez le monstre ! \n");
+
+	vieMonstre = vieMonstre - degat;
+
+	printf("PV du monstre: %d \n \n", vieMonstre);
 	
 	return 0;
 }
