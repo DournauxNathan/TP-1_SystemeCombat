a4e0a18cf6a6ecb2bb0534df2e5193cbd0ba0c72
DOURNAUX Nathan <dournauxnathan@gmail.com>
2020-01-21 13:55:26 +0100

    Choix monstre
    Tirer aléatoirement le comportement du monstre (attaque / défense).

--- game.c
+++ game.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h> //Ne pas oublier d'inclure le fichier time.h
 
 int main()
 {
@@ -7,24 +9,65 @@ int main()
 
 	int vieMonstre;
 	vieMonstre = 50;
+
 	float degat;
 	degat = 5;
 
+	int choix;
+
+	printf("\n");
+	printf("Un monstre sauvage apparait ! \n \n");
 
 	while(vieMonstre >= 0 || vie >= 0)
+	{
 		printf("Vous: %d \n", vie);
 
 		printf("Monstre: %d \n \n", vieMonstre);
 
-		printf("Vous attaquez le monstre ! \n");
+		//Action du joueur
+		printf("Attaquer [1] ou Se defendre [2]");
 
-		vieMonstre = vieMonstre - degat;
+		//Action du monstre
+		srand(time(NULL));
+    	int choixMonstre = rand()%3+1;    //entre 1-3
 
-		printf("PV du monstre: %d \n \n", vieMonstre);
+		scanf("%d", &choix);
+		printf("\n");
 
-		printf("Le monstre contre-attque ! \n");
+		switch (choix)
+		{
+			case 1 :
+				printf("Vous attaquez le monstre ! \n");
 
-		vie = vie - degat;
+				if (choixMonstre == 2)
+				{
+					printf("Le monstre se defend \n");		
+					vieMonstre = vieMonstre - (degat / 4);
+					printf("Votre n'attaque n'est pas tres efficace... \n");
+				}
+				else
+				{
+				vieMonstre = vieMonstre - degat;
+
+				printf("PV du monstre: %d \n \n", vieMonstre);
+
+				printf("C'est super efficace ! \n");
+				}
+			break;
+
+			case 2 :
+				printf("Vous vous defendez ! \n \n");
+
+				if (choixMonstre == 1)
+				{
+					degat = degat / 4;
+					vie = vie - degat;
+					printf("Vous venez de perdre %1f points de vie \n \n", degat);
+				}
+
+				
+			break;
+		}
 	}
 
 	return 0;
